339156b2e74c57a95c7a4ab7792c8eae11afb035
larsdeklijne <larsdeklijne@gmail.com>
2023-11-07 15:16:09 +0100

    created services section

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,13 @@
 import Button from "./Button";
 import Nav from "./Nav";
 import ShoeCard from "./ShoeCard";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
 
 export {
     Button,
     Nav,
     ShoeCard,
+    PopularProductCard,
+    ServiceCard,
 }
